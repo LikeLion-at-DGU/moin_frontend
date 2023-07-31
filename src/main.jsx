4791a3879e4d9f0c4c26d3d1a0ed68a58fb7c657
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2023-08-01 00:20:27 +0900

    :bug: Fix : router 연결 에러

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { RouterProvider } from "react-router-dom";
-import router from './Router.jsx';
+import { RouterProvider } from 'react-router-dom'
+import router from './router.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router ={router}/>
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
