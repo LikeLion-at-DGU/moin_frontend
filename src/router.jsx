b4b8a3f47e4f12d69a91e487605b3dcf7d42a0d1
심서현 <simseohyun@simseohyeon-ui-MacBookAir.local>
2023-07-31 14:03:19 +0900

    :memo: Update: router v6으로 수정
    router v6버전으로 수정했습니다. 현재 main, login 페이지로 이동 가능합니다.

--- src/router.jsx
+++ src/router.jsx
@@ -1,37 +1,45 @@
-import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import Main from "./pages/main/Main";
 import { styled } from "styled-components";
 import NavBar from "./components/layouts/NavBar";
 
+import { createBrowserRouter, Outlet } from "react-router-dom";
+import Error from "./pages/Error";
+import Login from "./pages/Auth/login";
+
 
 const Wrapper = styled.div`
-    margin: 0 auto;
-    width: 100%;
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
+  margin: 0 auto;
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
 `
 const Layout = () => {
-    return (
-      <>
-      <Wrapper>
-        <NavBar />
-        <Outlet />
-    </Wrapper>
-      </>
-    );
-  };
-
-export default function Router() {
   return (
-
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout/>}>
-            <Route index path="/" element={<Main/>} />
-        </Route>
-
-      </Routes>
-    </BrowserRouter>
+    <>
+    <Wrapper>
+      <NavBar />
+      <Outlet />
+    </Wrapper>
+    </>
   );
-}
+};
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children: [
+      { path:"",
+        element:<Main/>,
+      },
+      { path:"login",
+        element:<Login/>
+      },
+    ],
+    errorElement: <Error/>,
+  },
+]);
+
+export default router;
