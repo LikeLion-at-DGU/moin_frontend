7ca6be48557f6dce6f0850f7d542a082996d643a
심서현 <simseohyun@simseohyeon-ui-MacBookAir.local>
2023-08-04 00:07:08 +0900

    :sparkles: Feat: router 페이지 추가
    공지사항 건의사항 어바웃 추가했습니다.

--- src/components/layouts/NavBar.jsx
+++ src/components/layouts/NavBar.jsx
@@ -4,8 +4,11 @@ import { NavBarWrapper } from "./style";
 export default function NavBar() {
   return (
     <NavBarWrapper>
-      <Link to={`/`}>Moin</Link>
-      <Link to={`/community`}>커뮤니티입니다!!!</Link>
+      <Link to={`/`}>MO:IN</Link>
+      <Link to={`/community`}>커뮤니티</Link>
+      <Link to={`/notice`}>공지사항</Link>
+      <Link to={`/suggestion`}>건의사항</Link>
+      <Link to={`/about`}>About</Link>
       <Link to={`/login`}>Login</Link>
     </NavBarWrapper>
   );
---
+++ src/pages/notice/About.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Notice() {
+  return <>notice</>;
+}
+
+export default Notice;
