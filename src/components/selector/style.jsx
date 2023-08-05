e498325430733a8fb4c03a00ec76d3590a0da30b
심서현 <simseohyun@simseohyeon-ui-MacBookAir.local>
2023-08-05 17:31:28 +0900

    :lipstick: Style: banner Style
    bnner pagination.css 추가했습니다. posiotion relative로 바꾸고, active pagination primary 색으로 변경했습니다.

--- src/components/selector/style.jsx
+++ src/components/selector/style.jsx
@@ -4,8 +4,7 @@ export const SelectorSelect = styled.select`
   border: 1px solid;
   border-color: ${props => props.theme.colors.gray4};
   color: ${props => props.theme.colors.gray4};
-  background-color: ;
-  ${props => props.theme.colors.bg};
+  background-color: ${props => props.theme.colors.bg};
   border-radius: 5px;
   padding: 1rem;
 `;
