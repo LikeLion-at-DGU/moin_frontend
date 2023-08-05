ab801037c7c7520c03cc87fb88f5b0f9a4f05809
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-08-05 13:49:54 +0900

    🐛 fix : filename Auth

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { RouterProvider } from "react-router-dom";
-import router from "./router"
+import router from "./router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
