6310e9106c02f46793ba81a59431915fcdbbcdde
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-08-05 11:29:43 +0900

    :sparkles: Feat: login page 레이아웃
    feat: login page 레이아웃

--- src/pages/Auth/login.jsx
+++ src/pages/Auth/login.jsx
@@ -1,7 +1,68 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import * as S from "./style";
 
 function Login() {
-  return <>Login</>;
+  const navigate = useNavigate();
+  // const userInfo = useContext(AuthContext);
+  const [userInfo, setUserInfo] = useState(null);
+  const [email, setEmail] = useState("");
+
+  const [pwd, setPwd] = useState("");
+  const [isCreate, setIsCreate] = useState(false);
+  const [confirmPwd, setConfirmPwd] = useState("");
+  const [nickname, setNickname] = useState("");
+  const [phone, setPhone] = useState("");
+  const [passwordIsVaild, setpasswordIsVaild] = useState(false);
+  const [pwdMatchMessage, setPwdMatchMessage] = useState("");
+  const [isPhoneFormatValid, setPhoneFormatValid] = useState(true);
+
+  // (임시) 유저 정보 패치하기
+  // useEffect(() => {
+  //   setUserInfo(JSON.parse(localStorage.getItem("userInfo")));
+  // }, []);
+
+  return (
+    <>
+      <S.AuthWrapper>
+        {userInfo ? (
+          <>
+            <S.AuthText> {userInfo.email}님 환영합니다. </S.AuthText>
+            <S.AuthButton onClick={handleLogout}> 로그아웃 </S.AuthButton>
+          </>
+        ) : (
+          <S.AuthForm onSubmit={handleSubmit}>
+            {
+              <>
+                <LoginTitle />
+                <LoginForm
+                  handleEmail={handleEmail}
+                  handlePwd={handlePwd}
+                  email={email}
+                  pwd={pwd}
+                />
+              </>
+            }
+            <S.AuthButtonWrapper>
+              <S.AuthButton type="submit">
+                {isCreate ? "회원가입" : "로그인"}
+              </S.AuthButton>
+              {/* 구글 로그인 */}
+              <S.AuthSignUpButton type="submit" onClick={handleClickCreate}>
+                {isCreate ? "이미 계정이 있으신가요?" : "MOIN이 처음이신가요?"}
+              </S.AuthSignUpButton>
+              <S.AuthSocialButton onClick={handleGoogleLogin}>
+                <GoogleIcon />
+                <S.AuthSocialButtonText style={{ margin: "0 auto" }}>
+                  Google 로그인
+                </S.AuthSocialButtonText>
+              </S.AuthSocialButton>
+            </S.AuthButtonWrapper>
+          </S.AuthForm>
+        )}
+      </S.AuthWrapper>
+    </>
+  );
 }
 
 export default Login;
--- src/pages/Auth/style.jsx
+++ src/pages/Auth/style.jsx
@@ -1,1 +1,113 @@
-import { styled } from "styled-components";
+import { styled, keyframes } from "styled-components";
+
+const fadeInAnimation = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(-20px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
+export const AuthWrapper = styled.div`
+  animation: ${fadeInAnimation} 0.5s ease-in-out;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  flex-grow: 1;
+  padding-top: 100px;
+  margin-bottom: 100px;
+`;
+
+export const AuthText = styled.div`
+  text-align: center;
+  font-size: 36px;
+  font-weight: 700;
+  margin-bottom: 60px;
+`;
+export const AuthForm = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const AuthInputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 30px;
+`;
+export const AuthInput = styled.input`
+  display: flex;
+  flex-direction: column;
+  margin: 5px;
+  width: 557px;
+  height: 80px;
+  font-weight: 700;
+  padding-left: 31px;
+  font-size: 18px;
+  color: #ff5d47;
+  border-radius: 24px;
+  background: #f8f8fa;
+  border-radius: 24px;
+  &:focus {
+    border: 2px solid #ff5d47;
+  }
+`;
+
+export const AuthButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 50px;
+`;
+export const AuthButton = styled.button`
+  display: flex;
+  /* Orange */
+  padding: 30px;
+  justify-content: center; /* 수평 가운데 정렬 */
+  align-items: center; /* 수직 가운데 정렬 */
+
+  background: #ff5d47;
+  /* orange */
+  width: 562px;
+  height: 75px;
+  font-weight: 700;
+  font-size: 24px;
+  color: #ffffff;
+  box-shadow: 0px 1px 10px rgba(255, 93, 71, 0.7);
+  border-radius: 44px;
+  margin-bottom: 41px;
+`;
+export const AuthSignUpButton = styled.button`
+  display: flex;
+  /* Orange */
+  padding: 30px;
+  justify-content: center; /* 수평 가운데 정렬 */
+  align-items: center; /* 수직 가운데 정렬 */
+  width: 562px;
+  height: 75px;
+  font-weight: 700;
+  font-size: 24px;
+  color: #606067;
+`;
+
+export const AuthSocialButton = styled.button`
+  margin-top: 20px;
+  display: flex;
+  align-items: center;
+  padding: 0px 12px 0px 24px;
+  width: 398px;
+  height: 64px;
+  background: #ffffff;
+  box-shadow: 0px 0px 0px rgba(0, 0, 0, 0.08), 0px 1px 2px rgba(0, 0, 0, 0.25);
+  border-radius: 12px;
+  font-weight: 700;
+  font-size: 24px;
+`;
+
+export const AuthSocialButtonText = styled.p`
+  margin: 0 auto;
+`;
