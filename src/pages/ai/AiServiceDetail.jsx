00c7a4e949a1e0cdec2a5c3e434f7d5689cca707
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2023-08-05 17:02:59 +0900

    :sparkles: Feat: AI Service 디테일 페이지 백그라운드 배너 생성

--- src/pages/ai/AiServiceDetail.jsx
+++ src/pages/ai/AiServiceDetail.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import * as S from "./style";
 import { useLocation } from "react-router-dom";
 function AiServiceDetail() {
   const location = useLocation();
   const item = location.state.item;
-  return <>{item.name}</>;
+  return (
+    <>
+      {item.name}
+      <S.AiServiceDetailWrap>
+        <S.AiServiceDetailBanner></S.AiServiceDetailBanner>
+      </S.AiServiceDetailWrap>
+    </>
+  );
 }
 
 export default AiServiceDetail;
--- src/pages/ai/style.jsx
+++ src/pages/ai/style.jsx
@@ -1,1 +1,18 @@
 import { styled } from "styled-components";
+import colors from "../../style/theme";
+
+export const AiServiceDetailWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 10rem;
+  color: ${colors.black};
+`;
+
+export const AiServiceDetailBanner = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 47rem;
+  background-color: rgba(0, 10, 65, 0.97);
+`;
