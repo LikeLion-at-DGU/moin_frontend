8fc3377c67736461fcae058c16ef5825b085dcb6
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-08-05 11:37:47 +0900

    :sparkles: Feat: lgoin page 로그인 상태관리 기능 추가
    feat: lgoin page 로그인 상태관리 기능 추가

--- src/pages/Auth/login.jsx
+++ src/pages/Auth/login.jsx
@@ -6,21 +6,54 @@ function Login() {
   const navigate = useNavigate();
   // const userInfo = useContext(AuthContext);
   const [userInfo, setUserInfo] = useState(null);
-  const [email, setEmail] = useState("");
+  const [loginData, setLoginData] = useState({
+    email: "",
+    pwd: ""
+  });
 
-  const [pwd, setPwd] = useState("");
-  const [isCreate, setIsCreate] = useState(false);
-  const [confirmPwd, setConfirmPwd] = useState("");
-  const [nickname, setNickname] = useState("");
-  const [phone, setPhone] = useState("");
-  const [passwordIsVaild, setpasswordIsVaild] = useState(false);
-  const [pwdMatchMessage, setPwdMatchMessage] = useState("");
-  const [isPhoneFormatValid, setPhoneFormatValid] = useState(true);
+  // (임시) 유저 정보 LocalStorage에 저장하기
+  useEffect(() => {
+    setUserInfo(JSON.parse(localStorage.getItem("userInfo")));
+  }, []);
 
-  // (임시) 유저 정보 패치하기
-  // useEffect(() => {
-  //   setUserInfo(JSON.parse(localStorage.getItem("userInfo")));
-  // }, []);
+  // 로그인 입력정보 처리
+  const handleInputChange = event => {
+    const { name, value } = event.target;
+    setLoginData(prevData => ({
+      ...prevData,
+      [name]: value
+    }));
+  };
+
+  // 로그인 후 이동 페이지 -> 메인 페이지
+  const handleSubmit = event => {
+    event.preventDefault();
+    const { email, pwd } = loginData;
+
+    // 이메일, 비밀번호 입력 확인
+    if (email === "" || pwd === "") {
+      alert("이메일과 비밀번호를 입력해주세요.");
+      return;
+    }
+
+    // 로그인 성공 시
+    setUserInfo({
+      email: email,
+      pwd: pwd
+    });
+
+    // 로그인 정보 LocalStorage에 저장하기
+    localStorage.setItem(
+      "userInfo",
+      JSON.stringify({
+        email: email,
+        pwd: pwd
+      })
+    );
+
+    // 메인 페이지로 이동
+    navigate("/");
+  };
 
   return (
     <>
@@ -36,20 +69,16 @@ function Login() {
               <>
                 <LoginTitle />
                 <LoginForm
-                  handleEmail={handleEmail}
-                  handlePwd={handlePwd}
-                  email={email}
-                  pwd={pwd}
+                  handleInputChange={handleInputChange}
+                  loginData={loginData}
                 />
               </>
             }
             <S.AuthButtonWrapper>
-              <S.AuthButton type="submit">
-                {isCreate ? "회원가입" : "로그인"}
-              </S.AuthButton>
+              <S.AuthButton type="submit">로그인</S.AuthButton>
               {/* 구글 로그인 */}
               <S.AuthSignUpButton type="submit" onClick={handleClickCreate}>
-                {isCreate ? "이미 계정이 있으신가요?" : "MOIN이 처음이신가요?"}
+                MOIN이 처음이신가요
               </S.AuthSignUpButton>
               <S.AuthSocialButton onClick={handleGoogleLogin}>
                 <GoogleIcon />
