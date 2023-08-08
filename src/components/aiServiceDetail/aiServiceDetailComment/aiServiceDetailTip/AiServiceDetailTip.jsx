2a7d2a4e2d9a0fce13fcee6bf89fa49fa6e07b7f
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2023-08-08 19:51:42 +0900

    :sparkles: Feat: Ai Service 디테일 페이지 - 이용꿀팁 목록 데이터 추가

--- src/components/aiServiceDetail/aiServiceDetailComment/aiServiceDetailTip/AiServiceDetailTip.jsx
+++ src/components/aiServiceDetail/aiServiceDetailComment/aiServiceDetailTip/AiServiceDetailTip.jsx
@@ -1,10 +1,124 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import * as S from "./style";
 
 // 컴포넌트
 import Selector from "../../../selector/Selector";
 
 export function AiServiceDetailTip() {
+  const [tipContent, setTipContent] = useState([]);
+
+  useEffect(() => {
+    const tipData = [
+      {
+        id: 1,
+        title: "챗지피티에 대해 알아보자",
+        name: "Chat GPT",
+        date: "2023/01/01 23:00",
+        like: 17,
+        comment_cnt: 5
+      },
+      {
+        id: 2,
+        title: "챗지피티에 ",
+        name: "Chat GPT",
+        date: "2023/01/01 23:00",
+        like: 17,
+        comment_cnt: 5
+      },
+      {
+        id: 3,
+        title: "챗지피티에 대해 보자",
+        name: "Chat GPT",
+        date: "2023/01/01 23:00",
+        like: 17,
+        comment_cnt: 5
+      },
+      {
+        id: 4,
+        title: "챗지피티에 대해 알아보자",
+        name: "Chat GPT",
+        date: "2023/01/01 23:00",
+        like: 17,
+        comment_cnt: 5
+      },
+      {
+        id: 5,
+        title: "챗지피티에 대해 알아보자",
+        name: "Chat GPT",
+        date: "2023/01/01 23:00",
+        like: 17,
+        comment_cnt: 5
+      },
+      {
+        id: 6,
+        title: "챗지피티에 대해 알아보자",
+        name: "Chat GPT",
+        date: "2023/01/01 23:00",
+        like: 17,
+        comment_cnt: 5
+      },
+      {
+        id: 7,
+        title: "챗지피티에 대해 알아보자",
+        name: "Chat GPT",
+        date: "2023/01/01 23:00",
+        like: 17,
+        comment_cnt: 5
+      },
+      {
+        id: 8,
+        title: "챗지피티에",
+        name: "Chat GPT",
+        date: "2023/01/01 23:00",
+        like: 17,
+        comment_cnt: 5
+      },
+      {
+        id: 9,
+        title: "챗지피티에",
+        name: "Chat GPT",
+        date: "2023/01/01 23:00",
+        like: 17,
+        comment_cnt: 5
+      },
+      {
+        id: 10,
+        title: "챗지피티에",
+        name: "Chat GPT",
+        date: "2023/01/01 23:00",
+        like: 17,
+        comment_cnt: 5
+      },
+      {
+        id: 11,
+        title: "챗지피티에",
+        name: "Chat GPT",
+        date: "2023/01/01 23:00",
+        like: 17,
+        comment_cnt: 5
+      },
+      {
+        id: 12,
+        title: "챗지피티에 ",
+        name: "Chat GPT",
+        date: "2023/01/01 23:00",
+        like: 17,
+        comment_cnt: 5
+      },
+      {
+        id: 13,
+        title: "챗지피티에 해자",
+        name: "Chat GPT",
+        date: "2023/01/01 23:00",
+        like: 17,
+        comment_cnt: 5
+      }
+
+      // 추가.....
+    ];
+    setTipContent(tipData);
+  }, []);
+
   return (
     <>
       <S.AiServiceDetailTipWrap>
@@ -35,23 +149,28 @@ export function AiServiceDetailTip() {
             </S.AiServiceDetailTipTableTr>
           </S.AiServiceDetailTipTableThead>
           <S.AiServiceDetailTipTableTbody>
-            <S.AiServiceDetailTipTableTr>
-              <S.AiServiceDetailTipTableTd>1</S.AiServiceDetailTipTableTd>
-              <S.AiServiceDetailTipTableTd>
-                챗지피티에 대해 알아보자
-              </S.AiServiceDetailTipTableTd>
-              <S.AiServiceDetailTipTableTd>
-                Chat GPT
-              </S.AiServiceDetailTipTableTd>
-              <S.AiServiceDetailTipTableTd>
-                2023/01/01 23:00
-              </S.AiServiceDetailTipTableTd>
-              <S.AiServiceDetailTipTableTd>
-                <S.LikeIcon />
-                17
-                <S.CommentIcon />5
-              </S.AiServiceDetailTipTableTd>
-            </S.AiServiceDetailTipTableTr>
+            {tipContent.map(tip => (
+              <S.AiServiceDetailTipTableTr key={tip.id}>
+                <S.AiServiceDetailTipTableTd>
+                  {tip.id}
+                </S.AiServiceDetailTipTableTd>
+                <S.AiServiceDetailTipTableTd>
+                  {tip.title}{" "}
+                </S.AiServiceDetailTipTableTd>
+                <S.AiServiceDetailTipTableTd>
+                  {tip.name}
+                </S.AiServiceDetailTipTableTd>
+                <S.AiServiceDetailTipTableTd>
+                  {tip.date}
+                </S.AiServiceDetailTipTableTd>
+                <S.AiServiceDetailTipTableTd>
+                  <S.LikeIcon />
+                  {tip.like}
+                  <S.CommentIcon />
+                  {tip.comment_cnt}
+                </S.AiServiceDetailTipTableTd>
+              </S.AiServiceDetailTipTableTr>
+            ))}
           </S.AiServiceDetailTipTableTbody>
         </S.AiServiceDetailTipTable>
       </S.AiServiceDetailTipWrap>
