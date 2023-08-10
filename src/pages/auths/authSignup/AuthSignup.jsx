ae17c1c8787b489a9d493796b251b809f77f5c08
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-08-10 21:05:10 +0900

    :sparkles: Feat: 회원가입 기능
    Feat: 회원가입 기능

--- src/pages/auths/authSignup/AuthSignup.jsx
+++ src/pages/auths/authSignup/AuthSignup.jsx
@@ -1,7 +1,202 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import * as S from "./style";
+import { useNavigate } from "react-router-dom";
+
 function Signup() {
-  return <>Signup</>;
+  //유저 이메일, 직군, 비밀번호, 닉네임, 한줄소개 입력받음
+  // 이를 위한 유저 useState 객체 생성
+  const [user, setUser] = useState({
+    email: "",
+    job: "",
+    password: "",
+    nickname: "",
+    introduce: ""
+  });
+  const [pwd, setPwd] = useState("");
+  const [passwordIsVaild, setpasswordIsVaild] = useState(false);
+  const [pwdMatchMessage, setPwdMatchMessage] = useState("");
+  const [confirmPwd, setConfirmPwd] = useState("");
+
+  const [jobs, setJobs] = useState([]);
+
+  const jobList = [
+    "개발자",
+    "디자이너",
+    "기획자",
+    "마케터",
+    "영상/사진 작가",
+    "번역가",
+    "대학생",
+    "기타"
+  ];
+
+  const navigate = useNavigate();
+  useEffect(() => {
+    // 데이터 통신해서 API넣기
+    setJobs(jobList);
+  }, []);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    // 모든 필수 정보가 입력되었는지 확인
+    if (
+      user.email &&
+      user.password &&
+      confirmPwd &&
+      user.nickname &&
+      user.job
+    ) {
+      // use try catch to handle errors for POST request user Info
+      try {
+        // axios.post("/signup", user);
+        alert("회원가입이 완료되었습니다.");
+        navigate("/login");
+      } catch (error) {
+        alert("회원가입에 실패했습니다.");
+      }
+    } else {
+      alert("모든 필수 정보를 입력해주세요.");
+    }
+  };
+
+  // 비밀번호 입력
+  const handlePwd = e => {
+    e.preventDefault();
+    const password = e.target.value;
+    setPwd(password);
+    setUser({ ...user, password: password });
+
+    // Password strength validation
+    const hasMinLength = password.length >= 8;
+    const hasValidCombination =
+      /^(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&*()]).{2,}$/.test(password);
+    if (hasMinLength && hasValidCombination) {
+      setpasswordIsVaild(true);
+    } else {
+      setpasswordIsVaild(false);
+    }
+  };
+
+  // 비밀번호 확인 입력
+  const handleConfirmPwd = e => {
+    e.preventDefault();
+    const confirmedPassword = e.target.value;
+    setConfirmPwd(confirmedPassword);
+
+    // Password confirmation validation
+    if (confirmedPassword !== pwd) {
+      setPwdMatchMessage("동일하지 않은 비밀번호입니다 :(");
+    } else {
+      setPwdMatchMessage("확인 완료되었습니다 :)");
+    }
+  };
+
+  return (
+    <S.SignUpInputContainer onSubmit={handleSubmit}>
+      <S.SignUpInputWrapper>
+        {/* --------------- 이메일  --------------- */}
+        <S.SignUpInputTitleText>*이메일</S.SignUpInputTitleText>
+        <S.SignUpInput
+          required
+          placeholder="이메일을 입력해주세요."
+          type="email"
+          name="email"
+          onChange={e => setUser({ ...user, email: e.target.value })}
+          value={user.email}
+          isvaild="true"
+        />
+      </S.SignUpInputWrapper>
+      {/* --------------- 비밀번호 입력 --------------- */}
+      <S.SignUpInputWrapper>
+        <S.SignUpInputTitleText>*비밀번호</S.SignUpInputTitleText>
+        <S.SignUpInput
+          required
+          placeholder="비밀번호"
+          type="password"
+          name="pwd"
+          onChange={handlePwd}
+          value={pwd}
+          isvaild="true"
+        />
+        {pwd ? (
+          <S.MessageText isvaild={passwordIsVaild ? "true" : "false"}>
+            ︎✓ 8자 이상 입력
+            <br />✓ 숫자, 영문, 특수문자 포함하여, 2개 이상 조합{" "}
+          </S.MessageText>
+        ) : (
+          ""
+        )}
+      </S.SignUpInputWrapper>
+      {/* --------------- 비밀번호 확인 --------------- */}
+      <S.SignUpInputWrapper>
+        <S.SignUpInputTitleText>*비밀번호 확인</S.SignUpInputTitleText>
+        <S.SignUpInput
+          required
+          placeholder="비밀번호 확인"
+          type="password"
+          name="confirmPwd"
+          onChange={handleConfirmPwd}
+          value={confirmPwd}
+          isvaild={pwd === confirmPwd ? "true" : "false"}
+        />
+        {confirmPwd ? (
+          pwd === confirmPwd ? (
+            <S.MessageText isvaild="true">확인 완료</S.MessageText>
+          ) : (
+            <S.MessageText isvaild="false">
+              동일하지 않은 비밀번호입니다 :(
+            </S.MessageText>
+          )
+        ) : (
+          ""
+        )}
+      </S.SignUpInputWrapper>
+      {/* --------------- 닉네임 --------------- */}
+      <S.SignUpInputWrapper>
+        <S.SignUpInputTitleText>*닉네임</S.SignUpInputTitleText>
+        <S.SignUpInput
+          required
+          placeholder="닉네임을 입력해주세요."
+          type="text"
+          name="nickname"
+          onChange={e => setUser({ ...user, nickname: e.target.value })}
+          value={user.nickname}
+          isvaild="true"
+        />
+      </S.SignUpInputWrapper>
+      <S.SignUpInputWrapper>
+        <S.SignUpInputTitleText>*직군</S.SignUpInputTitleText>
+
+        <S.Select
+          required
+          name="job"
+          onChange={e => setUser({ ...user, job: e.target.value })}
+          value={user.job}
+        >
+          <S.Option value="" disabled>
+            직종을 선택해주세요.
+          </S.Option>
+          {jobs.map((job, index) => (
+            <S.Option key={index} value={job}>
+              {job}
+            </S.Option>
+          ))}
+        </S.Select>
+      </S.SignUpInputWrapper>
+      <S.SignUpInputWrapper>
+        <S.SignUpInputTitleText>한줄소개</S.SignUpInputTitleText>
+        <S.SignUpInput
+          placeholder="한줄소개를 입력해주세요."
+          type="text"
+          name="nickname"
+          onChange={e => setUser({ ...user, introduce: e.target.value })}
+          value={user.introduce}
+          isvaild="true"
+        />
+      </S.SignUpInputWrapper>
+      <S.AuthSignUpButton>가입하기</S.AuthSignUpButton>
+    </S.SignUpInputContainer>
+  );
 }
 
 export default Signup;
--- src/pages/auths/authSignup/style.jsx
+++ src/pages/auths/authSignup/style.jsx
@@ -1,1 +1,75 @@
 import { styled } from "styled-components";
+import colors from "../../../style/theme";
+
+export const SignUpInputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  margin-bottom: 60px;
+  width: 60%;
+`;
+
+/* ------- 회원 가입 컴포넌트 스타일 ------- */
+export const SignUpInputTitleText = styled.h2`
+  font-size: 20px;
+  font-weight: 700;
+  margin-bottom: 35px;
+`;
+
+export const SignUpInput = styled.input`
+  border: none;
+  border-bottom: 2px solid #aeafb9;
+  outline: none;
+  padding: 5px;
+  ::placeholder {
+    color: #aeafb9;
+  }
+  /* Your styling here */
+  border-bottom: ${props =>
+    props.isvaild == "true" ? "1px solid #000000" : "1px solid red"};
+  /* Additional styling based on validity */
+`;
+
+export const MessageText = styled.span`
+  margin-top: 10px;
+  color: ${props => (props.isvaild == "true" ? "#00C24E" : "#FF5D47")};
+  font-size: 13px;
+`;
+export const Select = styled.select`
+  /* 스타일링을 원하는대로 설정하세요 */
+  background-color: #f8f8fa;
+  border: none;
+  padding: 1rem;
+`;
+
+export const Option = styled.option`
+  /* 스타일링을 원하는대로 설정하세요 */
+`;
+
+export const SignUpInputContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 6rem;
+  // center
+  align-items: center;
+  justify-content: center;
+`;
+
+export const AuthSignUpButton = styled.button`
+  display: flex;
+  /* Orange */
+  padding: 2.6rem;
+  justify-content: center; /* 수평 가운데 정렬 */
+  align-items: center; /* 수직 가운데 정렬 */
+
+  background: ${colors.primary1};
+  /* orange */
+  width: 25rem;
+  height: 2rem;
+  font-weight: 600;
+  font-size: 2rem;
+  color: ${colors.bg};
+  box-shadow: 0rem 0.1rem 1rem ${colors.primary1};
+  border-radius: 4.4rem;
+  margin-bottom: 4.1rem;
+`;
