2b67d962de7fb92988c0a44cffdc38be8859b72c
심서현 <simseohyun@simseohyeon-ui-MacBookAir.local>
2023-08-11 18:15:44 +0900

    :sparkles: Feat: pagination 5회로 나누기
    페이지 5개씩 보여주고, < > 네비게이션 누르면 (n+1) *5 보여줍니다.

--- src/components/common/paging/Paging.jsx
+++ src/components/common/paging/Paging.jsx
@@ -1,18 +1,66 @@
-import React from "react";
-import Pagination from "react-js-pagination";
+import React, { useState, useEffect } from "react";
+import * as S from "./style";
 import "./style.css";
-const Paging = ({ page, count, postPerPage, setPage }) => {
+
+function Paging({ page, count, postPerPage, setPage }) {
+  const pageRangeDisplayed = 5;
+  const numPages = Math.ceil(count / postPerPage);
+  const numPagesSection = Math.ceil(numPages / pageRangeDisplayed);
+  const [currentPageSection, setCurrentPageSection] = useState(0);
+
+  console.log("numsPages", numPages);
+  console.log("numPagesSection", numPagesSection);
+  console.log("count", count);
+
+  useEffect(() => {
+    console.log(currentPageSection);
+    setPage(currentPageSection * pageRangeDisplayed + 1);
+  }, [currentPageSection]);
   return (
-    <Pagination
-      activePage={page}
-      itemsCountPerPage={postPerPage}
-      totalItemsCount={count}
-      pageRangeDisplayed={5}
-      prevPageText={"‹"}
-      nextPageText={"›"}
-      onChange={setPage}
-    />
+    <>
+      <S.Nav>
+        <S.Button
+          onClick={() => {
+            setCurrentPageSection(currentPageSection - 1);
+          }}
+          disabled={currentPageSection === 0}
+        >
+          &lt;
+        </S.Button>
+
+        {Array(numPages)
+          .fill()
+          .slice(
+            currentPageSection * pageRangeDisplayed,
+            (currentPageSection + 1) * pageRangeDisplayed
+          )
+          .map((_, i) => (
+            <S.Button
+              key={currentPageSection * pageRangeDisplayed + i + 1}
+              onClick={() =>
+                setPage(currentPageSection * pageRangeDisplayed + i + 1)
+              }
+              aria-current={
+                page === currentPageSection * pageRangeDisplayed + i + 1
+                  ? "page"
+                  : null
+              }
+            >
+              {currentPageSection * pageRangeDisplayed + i + 1}
+            </S.Button>
+          ))}
+
+        <S.Button
+          onClick={() => {
+            setCurrentPageSection(currentPageSection + 1);
+          }}
+          disabled={currentPageSection === numPagesSection - 1}
+        >
+          &gt;
+        </S.Button>
+      </S.Nav>
+    </>
   );
-};
+}
 
 export default Paging;
