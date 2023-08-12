a4479eb9a64feb84e5004586330f7a196ddb38dc
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-08-13 02:44:03 +0900

    :sparkles: Feat: 자유게시판 컴포넌트 반영
    feat: 자유게시판 컴포넌트 반영

---
+++ src/components/community/communityTips/CommunityTips.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import * as S from "./style";
+function CommunityTips() {
+  return <div>CommunityTips</div>;
+}
+
+export default CommunityTips;
--- src/pages/community/Community.jsx
+++ src/pages/community/Community.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import * as S from "./style";
+import * as AIS from "../ai/style";
 import Banner from "../../components/common/banner/Banner";
 import List from "../../components/common/list/List";
+import CommunityQna from "../../components/community/communityQna/CommunityQna";
+import CommunityCommon from "../../components/community/communityCommon/CommunityCommon";
+import CommunityTips from "../../components/community/communityTips/CommunityTips";
 
 function Community() {
+  // 탭 기능 구현
+  const [currentTab, setCurrentTab] = useState(0);
+
+  const tabContents = [
+    <CommunityCommon />,
+    <CommunityTips />,
+    <CommunityQna />
+  ];
+
+  const selectMenuHandler = index => {
+    setCurrentTab(index);
+  };
+
   return (
     <S.CommunityWrapper>
       <Banner
@@ -11,7 +28,33 @@ function Community() {
         titleEnglish="COMMUNITY"
         image={<S.CommunityIconImg />}
       />
-      {/* <List data={null} url={"/community/"} /> */}
+      <S.CommunityContentWrapper>
+        <AIS.AiServiceDetailCommentWrap>
+          <AIS.AiServiceDetailCommentCategory>
+            <AIS.AiServiceDetailCommentCategoryTabMenu>
+              <AIS.AiServiceDetailCommentCategoryMenuItem
+                isActive={currentTab === 0}
+                onClick={() => selectMenuHandler(0)}
+              >
+                자유 게시판
+              </AIS.AiServiceDetailCommentCategoryMenuItem>
+              <AIS.AiServiceDetailCommentCategoryMenuItem
+                isActive={currentTab === 1}
+                onClick={() => selectMenuHandler(1)}
+              >
+                이용꿀팁
+              </AIS.AiServiceDetailCommentCategoryMenuItem>
+              <AIS.AiServiceDetailCommentCategoryMenuItem
+                isActive={currentTab === 2}
+                onClick={() => selectMenuHandler(2)}
+              >
+                Q&A
+              </AIS.AiServiceDetailCommentCategoryMenuItem>
+            </AIS.AiServiceDetailCommentCategoryTabMenu>
+          </AIS.AiServiceDetailCommentCategory>
+          {tabContents[currentTab]}
+        </AIS.AiServiceDetailCommentWrap>
+      </S.CommunityContentWrapper>
     </S.CommunityWrapper>
   );
 }
