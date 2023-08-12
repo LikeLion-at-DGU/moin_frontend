5eaf1a9524dea9662adeadaba90cd36ebf30a64a
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-08-12 23:20:05 +0900

    :sparkles: Feat: logout 연동
    feat: logout 연동

--- src/pages/profile/profileMain/ProfileMain.jsx
+++ src/pages/profile/profileMain/ProfileMain.jsx
@@ -9,24 +9,43 @@ import MypageChat from "../../../assets/images/icon/mypageChat.png";
 import MypageVector from "../../../assets/images/icon/mypageVector.png";
 import AuthContentBox from "../../../components/auths/authContentBox/AuthContentBox";
 import { useNavigate } from "react-router-dom";
+import axios from "../../../api/axios";
 
 function ProfileMain() {
   // 유저 정보 불러오기
   const [userInfo, setUserInfo] = useRecoilState(userState);
 
   const navigate = useNavigate();
-  const handleLogout = () => {
-    // 로그아웃
-    setUserInfo(null);
+  const handleLogout = async () => {
+    const shouldLogout = window.confirm("로그아웃 하시겠습니까?");
+    if (!shouldLogout) {
+      return;
+    }
 
-    // 로컬 스토리지에서 로그인 정보 삭제
-    localStorage.removeItem("userInfo");
+    try {
+      const accessToken = userInfo.accessToken; // 추출한 accessToken
 
-    // 로그인 페이지로 이동
-    navigate("/");
+      const headers = {
+        Authorization: `Bearer ${accessToken}` // Bearer Token 설정
+      };
 
-    // // 페이지 새로고침
-    // window.location.reload();
+      const response = await axios.post("auth/logout/", null, {
+        headers
+      });
+
+      if (response.status === 200) {
+        alert("로그아웃 되었습니다.");
+        console.log(response);
+        setUserInfo(null);
+        // 로컬 스토리지에서 로그인 정보 삭제
+        localStorage.removeItem("userInfo");
+        // 로그인 페이지로 이동
+        navigate("/");
+      }
+    } catch (error) {
+      console.error("Error logging out:", error);
+      alert("로그아웃 실패했습니다.");
+    }
   };
 
   const handleChangePassword = () => {
