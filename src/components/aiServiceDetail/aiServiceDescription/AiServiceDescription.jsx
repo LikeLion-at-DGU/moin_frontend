49a10988c355b7680206d6d03433b3ec72dfba2d
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2023-08-13 20:34:42 +0900

    :lipstick: Style: Ai Serivce 디테일 페이지 - 서비스 소개 디자인

--- src/components/aiServiceDetail/aiServiceDescription/AiServiceDescription.jsx
+++ src/components/aiServiceDetail/aiServiceDescription/AiServiceDescription.jsx
@@ -1,5 +1,28 @@
 import React from "react";
+import * as S from "./style";
+
+// 아이콘
+import DescriptionHeartIcon from "../../../assets/images/icon/descriptionHeartIcon.png";
 
 export function AiServiceDescription() {
-  return <>기능 소개 탭입니다.</>;
+  return (
+    <>
+      <S.AiServiceDescriptionWrap>
+        <S.AiServiceDescriptionIntroTitle>
+          <S.AiServiceDescriptionIntroIcon
+            src={DescriptionHeartIcon}
+            alt="공유 아이콘"
+          />
+          서비스 소개
+        </S.AiServiceDescriptionIntroTitle>
+        <S.AiServiceDescriptionIntroContent>
+          Chat GPT는 인공지능 기반의 챗봇으로 사전에 학습한 데이터를 기반으로
+          하여 실시간 대화가 가능합니다. 난립식의 실문무터 긴 선명을 요구하는
+          정보, 전달식 답변, 창의적 아이디어에 대한 답변, 문장 완성, 등의 기능을
+          수행합니다. 차텔 및 혐오 발언을 차단하기 위한 특수 기능(모더라
+          (레이션AP)을 적용하여 기존 인공지능 챗봇보다 안전하게 사용 가능합니다.
+        </S.AiServiceDescriptionIntroContent>
+      </S.AiServiceDescriptionWrap>
+    </>
+  );
 }
---
+++ src/components/aiServiceDetail/aiServiceDescription/style.jsx
@@ -0,0 +1,45 @@
+import { styled } from "styled-components";
+
+export const AiServiceDescriptionWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 7.5rem;
+  margin-left: 3rem;
+`;
+
+export const AiServiceDescriptionIntroTitle = styled.div`
+  display: flex;
+  color: #4285f4;
+  text-align: center;
+  font-size: 2rem;
+  font-weight: 700;
+`;
+
+export const AiServiceDescriptionIntroIcon = styled.img`
+  display: flex;
+  width: 2.2rem;
+  height: 2.2rem;
+  margin-right: 1rem;
+  cursor: pointer;
+`;
+
+export const AiServiceDescriptionIntroContent = styled.div`
+  display: flex;
+  width: 100rem;
+  margin-top: 2.2rem;
+  margin-left: 3rem;
+  line-height: 3rem;
+  color: #282828;
+  font-size: 1.8rem;
+  font-weight: 500;
+  white-space: pre-line;
+  word-wrap: break-word;
+`;
+
+export const AiServiceDescriptionFunctionTitle = styled.div`
+  display: flex;
+`;
+
+export const AiServiceDescriptionFunctionContent = styled.div`
+  display: flex;
+`;
