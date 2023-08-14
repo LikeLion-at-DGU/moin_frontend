1939feb4f186b7ad91a16208a15a0abb9edcb813
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2023-08-14 18:50:15 +0900

    :sparkles: Feat: 인공지능 서비스 디테일 정보 불러오기 API 연동 - 추후 수정 필요

--- src/components/aiServiceDetail/aiServiceDescription/AiServiceDescription.jsx
+++ src/components/aiServiceDetail/aiServiceDescription/AiServiceDescription.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useLocation, useParams } from "react-router-dom";
 import * as S from "./style";
 
 // 아이콘
@@ -6,26 +7,25 @@ import DescriptionHeartIcon from "../../../assets/images/icon/descriptionHeartIc
 import DescriptionBulbIcon from "../../../assets/images/icon/descriptionBulbIcon.png";
 import DescriptionFunctionIcon from "../../../assets/images/icon/descriptionFunctionIcon.png";
 
+import axios from "../../../api/axios";
+
 export function AiServiceDescription() {
   const [data, setData] = useState([]);
+  const location = useLocation();
+  const aiName = decodeURI(location.pathname.split("/")[2]);
 
   useEffect(() => {
-    const descriptionData = {
-      ai: "ho",
-      introduce:
-        "Chat GPT는 인공지능 기반의 챗봇으로 사전에 학습한 데이터를 기반으로 하여 실시간 대화가 가능합니다. 난립식의 실문무터 긴 선명을 요구하는 정보, 전달식 답변, 창의적 아이디어에 대한 답변, 문장 완성,등의 기능을 수행합니다. 차텔 및 혐오 발언을 차단하기 위한 특수 기능(모더라 (레이션AP)을 적용하여 기존 인공지능 챗봇보다 안전하게 사용 가능합니다.",
-      header_1: "정보제공",
-      header_2: "콘텐츠 제작",
-      header_3: "코드 작성",
-      content_1:
-        "필요한 정보를 Chat GPT에 검색하여 정보를 수집합니다. 필요한 정보를 Chat GPT에 검색하여 정보를 수집합니다. 단답식 답변, 장문의 답변 등 다양한 형식의 답변을 제공합니다. \nChatGPT는 방대한 양의 텍스트 데이터로 학습되어 다양한 주제에 응답할 수 있습니다.",
-      content_2:
-        "필요한 정보를 Chat GPT에 검색하여 정보를 수집합니다. 단답식 답변, 장문의 답변 등 다양한 형식의 답변을 제공합니다. \nChatGPT는 방대한 양의 텍스트 데이터로 학습되어 다양한 주제에 응답할 수 있습니다.",
-      content_3:
-        "필요한 정보를 Chat GPT에 검색하여 정보를 수집합니다. 단답식 답변, 장문의 답변 등 다양한 형식의 답변을 제공합니다. \nChatGPT는 방대한 양의 텍스트 데이터로 학습되어 다양한 주제에 응답할 수 있습니다."
-    };
-    setData(descriptionData);
+    fetchData();
   }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(`/moin/detail/${aiName}/info`);
+      console.log("gd", response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   return (
     <>
--- src/pages/ai/AiServiceDetail.jsx
+++ src/pages/ai/AiServiceDetail.jsx
@@ -25,7 +25,7 @@ function AiServiceDetail() {
   const fetchData = async () => {
     try {
       const response = await axios.get(`/moin/detail/${aiName}`);
-      console.log("gd", response.data);
+      // console.log("gd", response.data);
 
       const detailData = response.data;
 
@@ -36,9 +36,9 @@ function AiServiceDetail() {
     }
   };
 
-  console.log("gdd", data);
-  console.log("gd2", introContent);
-  console.log(introContent);
+  // console.log("gdd", data);
+  // console.log("gd2", introContent);
+  // console.log(introContent);
 
   // 탭 기능 구현
   const [currentTab, setCurrentTab] = useState(0);
