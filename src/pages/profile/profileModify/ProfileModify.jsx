debbf034dd1e3120f0d25a8d678ae0f3e2252c0c
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-08-17 06:17:49 +0900

    :sparkles: Feat: 유저정보 수정 연동
    feat: 유저정보 수정 연동

--- src/pages/profile/profileModify/ProfileModify.jsx
+++ src/pages/profile/profileModify/ProfileModify.jsx
@@ -5,16 +5,18 @@ import MypageSetting from "../../../assets/images/icon/mypageSettingBlue.png";
 import * as S from "../../auths/authSignup/style";
 import { useLocation, useNavigate } from "react-router-dom";
 import axios from "../../../api/axios";
+import { useRecoilState } from "recoil";
+import { userState } from "../../../context/authState";
 
 function ProfileModify() {
   const { state } = useLocation();
+  const [userInfo, setUserInfo] = useRecoilState(userState);
+
   const [user, setUser] = useState({
     job: state.job,
     nickname: state.nickname,
     introduce: state.description
   });
-
-  console.log(state);
 
   const [jobs, setJobs] = useState([]);
 
@@ -39,9 +41,28 @@ function ProfileModify() {
     e.preventDefault();
     // 모든 필수 정보가 입력되었는지 확인
 
-    try {
-      alert("수정이 완료되었습니다.");
+    if (user.introduce.length > 50) {
+      alert("한줄소개는 50자 이내로 입력해주세요.");
+      return;
+    }
 
+    try {
+      const accessToken = userInfo.accessToken; // 추출한 accessToken
+      const headers = {
+        Authorization: `Bearer ${accessToken}` // Bearer Token 설정
+      };
+      const response = axios.patch(
+        "mypage/profile",
+        {
+          email: state.email,
+          nickname: user.nickname,
+          description: user.introduce,
+          job: user.job
+        },
+        { headers }
+      );
+
+      alert("수정이 완료되었습니다.");
       navigate("/mypage");
     } catch (error) {
       alert("수정에 실패했습니다.");
